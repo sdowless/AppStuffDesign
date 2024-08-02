906b2414c7ec99928cb458b94620e841bdcd8405
Stephan Dowless <slcd09@yahoo.com>
2024-08-02 15:36:14 -0400

    Initial Commit

---
+++ AppStuffDesign/AppStuffDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStuffDesignApp.swift
+//  AppStuffDesign
+//
+//  Created by Stephan Dowless on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStuffDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
