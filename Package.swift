3117401832176530e65685b66feecb8762771c5d
Stephan Dowless <slcd09@yahoo.com>
2024-08-06 13:15:49 -0400

    Added Kingfisher package dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "AppStuffDesign",
+    platforms: [.iOS(.v17), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AppStuffDesign",
             targets: ["AppStuffDesign"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/onevcat/Kingfisher",
+                .upToNextMajor(from: "7.12.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppStuffDesign"),
+            name: "AppStuffDesign",
+            dependencies: ["Kingfisher"]
+        ),
         .testTarget(
             name: "AppStuffDesignTests",
             dependencies: ["AppStuffDesign"]),
